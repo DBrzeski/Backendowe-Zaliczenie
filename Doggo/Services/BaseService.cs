e87c5161405a6a90e5e0657e8610aecc23eafbda
DBrzeski <brzeskirt@gmail.com>
2022-09-06 20:35:04 +0200

    v0.15: BaseServiceModel,Api request Changelog: -Added Base Service Class -Implemented generic methods for api requesting

--- Doggo/Services/BaseService.cs
+++ Doggo/Services/BaseService.cs
@@ -1,23 +1,83 @@
 ﻿using Doggo.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Doggo.Services
 {
     public class BaseService : IBaseService
     {
-        public ResponseDTO responseModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ResponseDTO responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            this.responseModel = new ResponseDTO();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("DoggoAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "aplication/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+                if(apiRequest.Data!= null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),Encoding.UTF8,"application/json");
+                }
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
+            }
+
+
+            catch (Exception ex)
+            {
+                var dto = new ResponseDTO
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDto;
+
+                
+            }
+        }
+
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            GC.SuppressFinalize(true);
         }
 
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
