377f7d02c36677e7bcb8490826fc08292af56530
DBrzeski <brzeskirt@gmail.com>
2022-09-06 20:08:10 +0200

    v0.13: ItemAPI, ApiBase, MainProject Changelog: -Added API base class -Added Item interface in main project

---
+++ Doggo/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.Models
+{
+    public class Item 
+    {
+
+        public int Id { get; set; }
+        //Basic information-----------------------------------
+        public string PictureUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        //Basic information-----------------------------------
+
+    }
+}
---
+++ Doggo/Services/IServices/IItemService.cs
@@ -0,0 +1,18 @@
+﻿using Doggo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.Services.IServices
+{
+    public interface IItemService
+    {
+        Task<T> GetAllItemsAsync<T>();
+        Task<T> GetAItemByIdAsync<T>(int id);
+        Task<T> CreateItemAsnyc<T>(Item item);
+        Task<T> UpdateItemAsnyc<T>(Item item);
+        Task<T> DeleteItemAsnyc<T>(int id);
+
+    }
+}
