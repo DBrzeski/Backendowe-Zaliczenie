945ed39498d77594d4f89b7c4e7b1dc445c63aea
DBrzeski <brzeskirt@gmail.com>
2022-09-06 02:41:36 +0200

    v0.08: ItemAPI, Automapper Changelog: -Added ItemRepository service -Added constructor -Implemented get request to display all items -Implemented get request to display items by ID

---
+++ Doggo.ProductAPI/Repositories/ItemRepository.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Doggo.ProductAPI.DbContexts;
+using Doggo.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.ProductAPI.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly AppDbContext _appdb;
+        private IMapper _mapper;
+
+        public ItemRepository(AppDbContext appDb, IMapper mapper)
+        {
+            _appdb = appDb;
+            _mapper = mapper;
+        }
+
+        public Task<bool> DeleteItem(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductDto> GetItemById(int itemId)
+        {
+            Item item = await _appdb.Item.Where( x => x.Id == itemId).FirstOrDefaultAsync();
+            return _mapper.Map<ProductDto>(item);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            List <Item> items =  await _appdb.Item.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(items);
+        }
+
+        public  Task<ProductDto> UpdateItem(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
