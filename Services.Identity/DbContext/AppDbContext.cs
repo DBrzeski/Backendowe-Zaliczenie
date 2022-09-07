675e0a06724c3322e2a7973c126c48c6c96835f1
DBrzeski <brzeskirt@gmail.com>
2022-09-07 23:36:51 +0200

    v0.25: Identiy microservice, DbContext Changelog: -Added DbContext for identity -Added Aplicationuser model

---
+++ Services.Identity/DbContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Doggo.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.Identity.DbContext
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
